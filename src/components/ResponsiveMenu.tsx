fff52a7893eabbfeb6780181cc83d0b43819eec7
Satria-WS <extencezone02@gmail.com>
2025-01-15 22:13:43 +0700

    enhance ResponsiveMenu with framer-motion for smooth animations

--- src/components/ResponsiveMenu.tsx
+++ src/components/ResponsiveMenu.tsx
@@ -1,6 +1,26 @@
-const ResponsiveMenu = () => {
-  return (
-    <div>ResponsiveMenu</div>
-  )
+
+import { motion, AnimatePresence } from "framer-motion";
+
+interface ResponsiveMenuProps {
+  open: boolean;
 }
-export default ResponsiveMenu
+
+const ResponsiveMenu = ({ open }: ResponsiveMenuProps) => {
+  return (
+    <>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className="fixed top-0 left-0 w-full h-full bg-black/60 z-10"
+          >
+            <div>test</div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
+export default ResponsiveMenu;
