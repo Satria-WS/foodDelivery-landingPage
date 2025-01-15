023717d411cbb8589256cb1019e0480daaa2c10c
Satria-WS <extencezone02@gmail.com>
2025-01-15 21:03:49 +0700

    add

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className="container">
+          {/* logo section */}
+          
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Navbar;
---
+++ src/data/navbarMenu.ts
@@ -0,0 +1,22 @@
+export const NavbarMenu = [
+  {
+    id: 1,
+    title: 'Home',
+    link:'#'
+  },
+  {
+    id: 2,
+    title: 'Menu',
+    link:'#'
+  },
+  {
+    id: 3,
+    title: 'Contact',
+    link:'#'
+  },
+  {
+    id: 4,
+    title: 'Shop',
+    link:'#'
+  },
+]
