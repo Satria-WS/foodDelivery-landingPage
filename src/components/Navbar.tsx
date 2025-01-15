57ed9b1289fd55debc107ba5118d2673dffd21be
Satria-WS <extencezone02@gmail.com>
2025-01-15 21:33:51 +0700

    enhance Navbar component with logo, menu, and search functionality

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,39 @@
+import { LuChefHat } from "react-icons/lu";
+import { CiSearch } from "react-icons/ci";
+import { NavbarMenu } from "../data/navbarMenu";
 const Navbar = () => {
   return (
     <>
       <nav>
         <div className="container">
-          {/* logo section */}
-          
+          <div className="flex items-center justify-between py-8">
+            {/* logo section */}
+            <div className="text-2xl flex items-center gap-2 font-bold uppercase">
+              <LuChefHat />
+              <p>Foo</p>
+            </div>
+            {/* menu section */}
+            <div className="hidden md:block">
+              <ul className="flex items-center gap-6 text-gray-600">
+                {NavbarMenu.map((item, index) => (
+                  <li key={index}>
+                    <a
+                      className="inline-block py-1 px-3 hover:text-secondary font-semibold"
+                      href={item.link}
+                    >
+                      {item.title}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* search section */}
+            <div>
+              <button className="text-2xl hover:bg-secondary hover:text-white rounded-full duration-200 p-2">
+                <CiSearch />
+              </button>
+            </div>
+          </div>
         </div>
       </nav>
     </>
